25a5fc8d8d836dfffaaaf261c7e7441fc030e651
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-07-29 21:25:10 +0300

    Global Exception Handling

---
+++ PasswordManager/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordManager.Exceptions;
+using System.Net;
+using System.Net.Mail;
+using System.Text.Json;
+
+namespace PasswordManager.Middlewares
+{
+    
+    public class GlobalExceptionHandler : IMiddleware
+    {
+        // TODO: DI Logger
+
+        readonly static string ServerError = "Server Error";
+        readonly static string UserError = "User Error";
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (EmailAlreadyExistsException e)
+            {
+           
+
+                UpdateHttpContext(
+                    context,
+                    (int)HttpStatusCode.Conflict,
+                    UserError,
+                    "Email already exists",
+                    "The provided email address is already registered in our system."
+                );
+            }
+            catch (Exception e)
+            {
+               
+
+                UpdateHttpContext(
+                    context,
+                    (int)HttpStatusCode.InternalServerError,
+                    ServerError,
+                    "Server Error",
+                    "An internal server error has occured!"
+                );    
+            }
+        }
+
+        private async void UpdateHttpContext(HttpContext context, int statusCode, string type, string title, string detail)
+        {
+            context.Response.StatusCode = statusCode;
+            context.Response.ContentType = "application/json";
+
+            ProblemDetails problem = new ProblemDetails()
+            {
+                Status = statusCode,
+                Type = type,
+                Title = title,
+                Detail = detail
+            };
+            string problemJson = JsonSerializer.Serialize(problem);
+
+            await context.Response.WriteAsync(problemJson);
+        }
+    }
+
+    
+}
