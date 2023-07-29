45c9224c50d3f5333e8a1e394483733f0fc66414
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-07-29 21:38:20 +0300

    User mapper

---
+++ PasswordManager/Mappers/UserMapper.cs
@@ -0,0 +1,32 @@
+﻿using PasswordManager.Contracts;
+using PasswordManager.Models;
+using System.Text;
+
+namespace PasswordManager.Mappers
+{
+    public class UserMapper
+    {
+        public static User ToUser(string email)
+        {
+            return new User()
+            {
+                Id = Guid.NewGuid(),
+                CommunicationAddress = email,
+                Active = false,
+                Created = DateTime.UtcNow,
+                LastUpdated = DateTime.UtcNow
+            };
+        }
+
+        public static UserResponse ToUserResponse(User user)
+        {
+            return new UserResponse(
+                user.Id,
+                user.CommunicationAddress,
+                user.Active,
+                user.Created,
+                user.LastUpdated
+            );
+        }
+    }
+}
