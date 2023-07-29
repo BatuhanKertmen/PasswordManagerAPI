1e5386e059bc96377116dbaf3f509a710c1d6217
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-07-29 22:21:46 +0300

    typo

--- PasswordManager/Database/AppDbContext.cs
+++ PasswordManager/Database/AppDbContext.cs
@@ -5,20 +5,20 @@ namespace PasswordManager.Database
 {
     public class AppDbContext : DbContext
     {
-        protected readonly IConfiguration configuration;
+        private readonly IConfiguration _configuration;
 
         public AppDbContext(IConfiguration configuration)
         {
-            this.configuration = configuration;
+            _configuration = configuration;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql(configuration.GetConnectionString("PostgresConnectionString"));
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("PostgresConnectionString"));
         }
 
         public DbSet<User> Users { get; set; }
         public DbSet<ActivationCode> ActivationCodes { get; set; }
-        public DbSet<LoginInformation> LoginInformations { get; set; }
+        public DbSet<LoginInformation> LoginInformation { get; set; }
     }
 }
