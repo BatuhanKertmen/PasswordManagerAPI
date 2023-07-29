953327e92ebffe34fa017d7ae4ad58e0279b51a6
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-07-29 21:33:12 +0300

    db context

---
+++ PasswordManager/Databsae/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PasswordManager.Models;
+
+namespace PasswordManager.Database
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(configuration.GetConnectionString("PostgresConnectionString"));
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<ActivationCode> ActivationCodes { get; set; }
+        public DbSet<LoginInformation> LoginInformations { get; set; }
+    }
+}
