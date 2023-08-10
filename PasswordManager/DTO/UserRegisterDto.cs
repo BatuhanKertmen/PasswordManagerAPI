0101e49bd14697abdf1ca1c2338b667e0b84dcfd
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-08-10 23:25:55 +0300

    Using Dto instead of contracts automapper

---
+++ PasswordManager/DTO/UserRegisterDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.DTO;
+
+public class UserRegisterDto
+{
+    [Required]
+    public string CommunicationAddress { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
