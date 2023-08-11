968ca544f6a872ac76a4979b251aa2661e61a85b
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-08-11 13:40:30 +0300

    jwt token creation

---
+++ PasswordManager/Services/JwtService.cs
@@ -0,0 +1,37 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using PasswordManager.Models;
+
+namespace PasswordManager.Services;
+
+public class JwtService : IJwtService
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+
+    public string CreateJwtToken(User user)
+    {
+        var claims = new List<Claim>();
+        claims.Add(new Claim(ClaimTypes.PrimarySid, user.Id.ToString()));
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetValue<string>("JWT:key")));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            _configuration.GetValue<string>("JWT:issuer"),
+            _configuration.GetValue<string>("JWT:audience"),
+            claims,
+            expires: DateTime.UtcNow.AddMinutes(15),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
