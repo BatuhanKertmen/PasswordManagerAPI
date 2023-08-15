e1b0f4f9c13f1a7657cb27818e43abbba5b6ab60
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-08-15 19:44:11 +0300

    dto and mapper

---
+++ PasswordManager/DTO/LoginInformationRequestDto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.DTO;
+
+public class LoginInformationRequestDto
+{
+    [Required]
+    public string UserName { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    [Required]
+    public string Domain { get; set; }
+    
+    [Required]
+    [RegularExpression("[0-9A-F]64")]
+    public string SaltHexString { get; set; }
+    
+    [Required]
+    [Range(1,1024)]
+    public int MemorySize { get; set; }
+
+    [Required]
+    [Range(1, 32)]
+    public int DegreeOfParallelism { get; set; }
+
+    [Required]
+    [Range(1, 32)]
+    public int Iterations { get; set; }
+}
---
+++ PasswordManager/DTO/LoginInformationResponseDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordManager.DTO;
+
+public class LoginInformationResponseDto
+{
+    [Required]
+    public string Domain { get; set; }
+}
