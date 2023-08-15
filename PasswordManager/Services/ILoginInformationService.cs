3501e7d581b9de6f70b20c32620b962490877797
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-08-15 19:47:02 +0300

    login info services

---
+++ PasswordManager/Services/ILoginInformationService.cs
@@ -0,0 +1,8 @@
+﻿using PasswordManager.Models;
+
+namespace PasswordManager.Services;
+
+public interface ILoginInformationService
+{
+    Task<LoginInformation> SaveAsync(LoginInformation loginInformation);
+}
---
+++ PasswordManager/Services/LoginInformationPasswordService.cs
@@ -0,0 +1,21 @@
+﻿using PasswordManager.Database;
+using PasswordManager.Models;
+using PasswordManager.Services;
+
+namespace PasswordManager.Repositories;
+
+public class LoginInformationPasswordService : ILoginInformationPasswordService
+{
+    private readonly ILoginInformationPasswordRepository _loginInformationPasswordRepository;
+
+    public LoginInformationPasswordService(ILoginInformationPasswordRepository loginInformationPasswordRepository)
+    {
+        _loginInformationPasswordRepository = loginInformationPasswordRepository;
+    }
+
+    public async Task<LoginInformationPassword> SaveAsync(LoginInformationPassword loginInformationPassword)
+    {
+        loginInformationPassword = await _loginInformationPasswordRepository.SaveAsync(loginInformationPassword);
+        return loginInformationPassword;
+    }
+}
