0dccfc25b19c7f1377100889b6d51034b1586b98
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-08-22 20:17:04 +0300

    register request dto updated

--- PasswordManager/DTO/UserRegisterRequestDto.cs
+++ PasswordManager/DTO/UserRegisterRequestDto.cs
@@ -9,9 +9,22 @@ public class UserRegisterRequestDto
     public string CommunicationAddress { get; set; }
     
     [Required(ErrorMessage = "Password is required")]
-    [StringLength(64, MinimumLength = 8, ErrorMessage = "Password must be between 8 and 64 characters")]
-    [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", 
-        ErrorMessage = "Password must contain at least one letter, one number, and one special character")]
-    [DataType(DataType.Password)]
-    public string Password { get; set; }
+    [RegularExpression("[a-zA-Z0-9]{256}", ErrorMessage = "Password 128 byte hex-string")]
+    public string PasswordHexString { get; set; }
+    
+    [Required(ErrorMessage = "Salt is required")]
+    [RegularExpression("[a-zA-Z0-9]{64}", ErrorMessage = "Salt 32 byte hex-string")]
+    public string SaltHexString { get; set; }
+    
+    [Required]
+    [Range(1,1024, ErrorMessage = "Given Memory size is not in the accepted range")]
+    public int MemorySize { get; set; }
+
+    [Required]
+    [Range(1, 32, ErrorMessage = "Given parallel degree is not in the accepted range")]
+    public int DegreeOfParallelism { get; set; }
+
+    [Required]
+    [Range(1, 32, ErrorMessage = "Given iterations is not in the accepted range")]
+    public int Iterations { get; set; }
 }
