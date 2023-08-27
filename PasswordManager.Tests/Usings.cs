fc8a628cfa2dc239cc32c527df986a9c61c3c261
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-08-27 21:38:54 +0300

    usings

--- PasswordManager.Tests/Usings.cs
+++ PasswordManager.Tests/Usings.cs
@@ -1,12 +1,23 @@
-global using Xunit;
-global using PasswordManager.Repositories;
-global using Moq;
-global using PasswordManager.Exceptions;
-global using PasswordManager.Models;
-global using PasswordManager.Services;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using System.Text;
+
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
 global using Microsoft.EntityFrameworkCore;
-global using Microsoft.Extensions.Configuration;
-global using PasswordManager.Database;
+
 global using AutoMapper;
+
+global using Moq;
+global using Xunit;
+
+global using PasswordManager.Communications;
+global using PasswordManager.Database;
 global using PasswordManager.DTO;
+global using PasswordManager.Exceptions;
 global using PasswordManager.Facades;
+global using PasswordManager.Repositories;
+global using PasswordManager.Models;
+global using PasswordManager.Secrets;
+global using PasswordManager.Secrets.FileBased;
+global using PasswordManager.Services;
