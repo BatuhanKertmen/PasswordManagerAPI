e9363a0b965dc7f205055b536acf60d81ed8e69d
Batuhan <batuhankertmen@sabanciuniv.edu>
2023-08-27 21:38:43 +0300

    migrations

---
+++ PasswordManager/Migrations/20230827105256_User Password Typo.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PasswordManager.Migrations
+{
+    public partial class UserPasswordTypo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DegreeOfParallism",
+                table: "UserPasswords",
+                newName: "DegreeOfParallelism");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DegreeOfParallelism",
+                table: "UserPasswords",
+                newName: "DegreeOfParallism");
+        }
+    }
+}
