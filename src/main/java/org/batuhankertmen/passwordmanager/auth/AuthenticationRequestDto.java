4a7cccc1d520c1569fdcd7b5e35e774e5902bb2e
Batuhan <batuhankertmen@sabanciuniv.edu>
2024-08-16 01:02:32 +0300

    remember me logic

--- src/main/java/org/batuhankertmen/passwordmanager/auth/AuthenticationRequestDto.java
+++ src/main/java/org/batuhankertmen/passwordmanager/auth/AuthenticationRequestDto.java
@@ -15,10 +15,12 @@ import lombok.NoArgsConstructor;
 public class AuthenticationRequestDto {
 
     @NotBlank(message = "Username is mandatory")
-    @Size(min = 5, max = 50, message = "Username must be between 5 and 50 characters")
+    @Size(min = 5, max = 64, message = "Username must be between 5 and 64 characters")
     private String username;
 
     @NotBlank(message = "Password is mandatory")
     @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
     private String password;
+
+    private boolean rememberMe;
 }
