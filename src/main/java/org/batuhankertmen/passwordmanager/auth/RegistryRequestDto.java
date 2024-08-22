5512fbb632cba70ff0c40aef1041fe663c7ec745
Batuhan <batuhankertmen@sabanciuniv.edu>
2024-08-22 19:09:37 +0300

    small fix

--- src/main/java/org/batuhankertmen/passwordmanager/auth/RegistryRequestDto.java
+++ src/main/java/org/batuhankertmen/passwordmanager/auth/RegistryRequestDto.java
@@ -1,5 +1,6 @@
 package org.batuhankertmen.passwordmanager.auth;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -14,10 +15,12 @@ import lombok.NoArgsConstructor;
 public class RegistryRequestDto {
     @NotBlank(message = "Username is mandatory")
     @Size(min = 5, max = 64, message = "Username must be between 5 and 64 characters")
+    @JsonProperty("first_name")
     private String firstName;
 
     @NotBlank(message = "Username is mandatory")
     @Size(min = 5, max = 64, message = "Username must be between 5 and 64 characters")
+    @JsonProperty("last_name")
     private String lastName;
 
     @NotBlank(message = "Username is mandatory")
